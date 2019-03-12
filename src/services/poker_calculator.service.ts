d032d0f809d042ecd1f73714fc1a875e54ea411a
Manuel Trambert <m.trambert@gmail.com>
2019-03-12 09:24:15 +0100

    vs calculator

--- src/services/poker_calculator.service.ts
+++ src/services/poker_calculator.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@nestjs/common';
-import {listCards} from '../config/cards';
-import {calculateEquity} from 'poker-odds'
+import { Injectable } from '@nestjs/common';
+import { listCards } from '../config/cards';
+import { calculateEquity } from 'poker-odds'
 import { type } from 'os';
 
 @Injectable()
@@ -35,7 +35,7 @@ export class PokerCalculatorService {
                 const resCardOne = listCards.find((card) => {
                     return card.id === cards[i];
                 });
-        
+
                 const resCardSecond = listCards.find((card) => {
                     return card.id === cards[i + 1];
                 });
@@ -43,7 +43,7 @@ export class PokerCalculatorService {
                 hands.push([resCardOne.letterName + resCardOne.letterType, resCardSecond.letterName + resCardSecond.letterType]);
             }
         }
-    
+
         return await calculateEquity(hands, boardCalculator);
     }
 
@@ -72,7 +72,7 @@ export class PokerCalculatorService {
         });
 
         let adverse = [];
-        if (resCardOne.id !== 8  && resCardSecond.id !== 8) {
+        if (resCardOne.id !== 8 && resCardSecond.id !== 8) {
             adverse[0] = 'Qh';
         } else if (resCardOne.id !== 9 && resCardSecond.id !== 9) {
             adverse[0] = 'Qs'
@@ -80,7 +80,7 @@ export class PokerCalculatorService {
             adverse[0] = 'Qc'
         }
 
-        if (resCardOne.id !== 26  && resCardSecond.id !== 26) {
+        if (resCardOne.id !== 26 && resCardSecond.id !== 26) {
             adverse[1] = '8c';
         } else if (resCardOne.id !== 27 && resCardSecond.id !== 27) {
             adverse[1] = '8d'
@@ -89,6 +89,17 @@ export class PokerCalculatorService {
         }
 
         const result = await calculateEquity([[resCardOne.letterName + resCardOne.letterType, resCardSecond.letterName + resCardSecond.letterType], adverse], boardCalculator);
+        if (resCardOne.letterName === resCardSecond.letterName) {
+            result[0].advice = 'Vous pouvez jouer de manière agressive ici votre jeu est assez fort.';
+        } else if (resCardOne.letterType === resCardSecond.letterType && resCardOne.id < 20 && resCardSecond.id < 20) {
+            result[0].advice = 'Vous pouvez jouer cette main mais avec prudence il est possible que vous ne touchiez rien sur le board et votre adversaire une paire. Jouez de manière réfléchie et n\'engagez pas plus de 10% de votre stack';;
+        } else if (resCardOne.id < 16 && resCardSecond.id < 16) {
+            result[0].advice = 'Vous avez une belle main ici mais beaucoup de chance de ne rien avoir sur le board. Jouez au maximum 5% de votre stack';;
+        } else if ((resCardOne.id < 12 || resCardSecond.id < 12)) {
+            result[0].advice = 'Celà pourrait être une belle main mais dépend énormément de ce qu\'il va tomber sur le board. Attendez de voir comment vos adversaires vont jouer. N\'engagez pas plus de 2% de votre stack';
+        } else {
+            result[0].advice = 'Mis à part si vous avez déjà misé la grosse blind, ou vous êtes au bouton et souhaitez tenter un bluff il faut vous coucher.';
+        }
         return result;
     }
 }
